f594fe27c22774ef45e91cb64e47a43984ee53ce
zhang-hongbin <474572465@qq.com>
2018-04-10 22:44:21 +0800

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+ReactDOM.render(
+    <h1>Hello World 1111122</h1>,
+    document.getElementById("example")
+);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: __dirname + "/src/js/index.js",
+  output: {
+    path: __dirname + "/src/js",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+};
